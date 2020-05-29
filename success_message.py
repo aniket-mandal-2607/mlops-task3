9b228c17974589b5be87fed42719a7a2d56a2f82
aniket-mandal-2607 <63110858+aniket-mandal-2607@users.noreply.github.com>
2020-05-29 21:14:06 +0530

    Create success_message.py

---
+++ success_message.py
@@ -0,0 +1,16 @@
+import smtplib, ssl
+import getpass
+port = 587  # For starttls
+smtp_server = "smtp.gmail.com"
+sender_email = "mandal.a.2607@gmail.com"
+receiver_email = "srock350sd@gmail.com"
+password =getpass.getpass('enter pass ->')
+message = "Model training SUCCESSFUL"
+
+context = ssl.create_default_context()
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)
+    server.ehlo()  # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
